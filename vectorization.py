5dc2a4fb90f81ba04ab0e1f5b94d5fed2a89b102
BrainNim <hhhsss0815@gmail.com>
2024-04-14 13:45:45 +0000

    벡터저장소 로컬 저장 빈도 변경

--- vectorization.py
+++ vectorization.py
@@ -43,15 +43,17 @@ bedrock_embeddings = BedrockEmbeddings(model_id='amazon.titan-embed-text-v1', cl
 
 
 # 벡터 임베딩
-start_num = 10
+start_num = 7748
 for i in tqdm(range(start_num, len(docs))):
     chunk_doc = [docs[i]]  # docs는 list형태여야 함
     if i == 0:
         vector = FAISS.from_documents(chunk_doc, bedrock_embeddings)
     else:
         add_to_store(docs=chunk_doc, original_store=vector, embeddings=bedrock_embeddings)
-    vector.save_local("faiss_index")  # 로컬에 저장
-    vector = FAISS.load_local("faiss_index", bedrock_embeddings, allow_dangerous_deserialization=True)  # 로컬에 저장한 벡터 불러오기
+
+    if i % 100 == 0:
+        vector.save_local("faiss_index")  # 로컬에 저장
+        vector = FAISS.load_local("faiss_index", bedrock_embeddings, allow_dangerous_deserialization=True)  # 로컬에 저장한 벡터 불러오기
 
 
 # 결과 확인
